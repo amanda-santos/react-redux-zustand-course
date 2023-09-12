6ce9401d7163376862ecdd534526b226f7eec333
amanda-santos <amandasf0200@gmail.com>
2023-09-12 18:11:54 -0300

    refactor: ♻️ create global hook

--- src/pages/Player.tsx
+++ src/pages/Player.tsx
@@ -1,14 +1,22 @@
+import { useEffect } from "react";
 import { MessageCircle } from "lucide-react";
 
 import { Header } from "../components/Header";
 import { Video } from "../components/Video";
 import { Module } from "../components/Module";
 import { useAppSelector } from "../store";
+import { useCurrentLesson } from "../store/slices/player";
 
 export function Player() {
   const modules = useAppSelector(state => {
     return state.player.course.modules
   })
+
+  const { currentLesson } = useCurrentLesson()
+
+  useEffect(() => {
+    document.title = `Assistindo: ${currentLesson.title}`
+  }, [currentLesson]);
 
   return (
     <div className="h-screen bg-zinc-950 text-zinc-50 flex justify-center items-center">
