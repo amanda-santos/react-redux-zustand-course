a32643ce05eb3a4a96742c7cb73d84c904939743
amanda-santos <amandasf0200@gmail.com>
2023-10-04 19:23:23 -0300

    feat: ✨ add loading state

--- src/store/slices/__tests__/player.test.ts
+++ src/store/slices/__tests__/player.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, it } from 'vitest'
+import { describe, expect, it } from "vitest";
 
 import { player as reducer, play, next, PlayerState } from "../player";
 
@@ -8,59 +8,74 @@ const exampleState: PlayerState = {
     modules: [
       {
         id: 1,
-        title: 'Iniciando com React',
+        title: "Iniciando com React",
         lessons: [
-          { id: 'Jai8w6K_GnY', title: 'CSS Modules', duration: '13:45' },
-          { id: 'w-DW4DhDfcw', title: 'Estilização do Post', duration: '10:05' },
+          { id: "Jai8w6K_GnY", title: "CSS Modules", duration: "13:45" },
+          {
+            id: "w-DW4DhDfcw",
+            title: "Estilização do Post",
+            duration: "10:05",
+          },
         ],
       },
       {
         id: 2,
-        title: 'Estrutura da aplicação',
+        title: "Estrutura da aplicação",
         lessons: [
-          { id: 'gE48FQXRZ_o', title: 'Componente: Comment', duration: '13:45' },
-          { id: 'Ng_Vk4tBl0g', title: 'Responsividade', duration: '10:05' },
+          {
+            id: "gE48FQXRZ_o",
+            title: "Componente: Comment",
+            duration: "13:45",
+          },
+          { id: "Ng_Vk4tBl0g", title: "Responsividade", duration: "10:05" },
         ],
       },
     ],
   },
+  isLoading: false,
   currentModuleIndex: 0,
   currentLessonIndex: 0,
-}
+};
 
-describe('player slice', () => {
-  it('should be able to play', () => {
-    const state = reducer(exampleState, play([1, 2]))
+describe("player slice", () => {
+  it("should be able to play", () => {
+    const state = reducer(exampleState, play([1, 2]));
 
-    expect(state.currentModuleIndex).toEqual(1)
-    expect(state.currentLessonIndex).toEqual(2)
-  })
+    expect(state.currentModuleIndex).toEqual(1);
+    expect(state.currentLessonIndex).toEqual(2);
+  });
 
-  it('should be able to play next video automatically', () => {
-    const state = reducer(exampleState, next())
+  it("should be able to play next video automatically", () => {
+    const state = reducer(exampleState, next());
 
-    expect(state.currentModuleIndex).toEqual(0)
-    expect(state.currentLessonIndex).toEqual(1)
-  })
+    expect(state.currentModuleIndex).toEqual(0);
+    expect(state.currentLessonIndex).toEqual(1);
+  });
 
-  it('should be able to jump to the next module automatically', () => {
-    const state = reducer({
-      ...exampleState,
-      currentLessonIndex: 1
-    }, next())
+  it("should be able to jump to the next module automatically", () => {
+    const state = reducer(
+      {
+        ...exampleState,
+        currentLessonIndex: 1,
+      },
+      next()
+    );
 
-    expect(state.currentModuleIndex).toEqual(1)
-    expect(state.currentLessonIndex).toEqual(0)
-  })
+    expect(state.currentModuleIndex).toEqual(1);
+    expect(state.currentLessonIndex).toEqual(0);
+  });
 
-  it('should not update the current module and lesson index if there is not next lesson available', () => {
-    const state = reducer({
-      ...exampleState,
-      currentModuleIndex: 1,
-      currentLessonIndex: 1
-    }, next())
+  it("should not update the current module and lesson index if there is not next lesson available", () => {
+    const state = reducer(
+      {
+        ...exampleState,
+        currentModuleIndex: 1,
+        currentLessonIndex: 1,
+      },
+      next()
+    );
 
-    expect(state.currentModuleIndex).toEqual(1)
-    expect(state.currentLessonIndex).toEqual(1)
-  })
+    expect(state.currentModuleIndex).toEqual(1);
+    expect(state.currentLessonIndex).toEqual(1);
+  });
 });
--- src/store/slices/player.ts
+++ src/store/slices/player.ts
@@ -4,50 +4,52 @@ import { useAppSelector } from "..";
 import { api } from "../../lib/axios";
 
 interface Course {
-  id: number
+  id: number;
   modules: Array<{
-    id: number
-    title: string
+    id: number;
+    title: string;
     lessons: Array<{
-      id: string
-      title: string
-      duration: string
-    }>
-  }>
+      id: string;
+      title: string;
+      duration: string;
+    }>;
+  }>;
 }
 
 export interface PlayerState {
   course: Course | null;
   currentModuleIndex: number;
   currentLessonIndex: number;
+  isLoading: boolean;
 }
 
 const initialState: PlayerState = {
   course: null,
   currentModuleIndex: 0,
   currentLessonIndex: 0,
-}
+  isLoading: false,
+};
 
-export const loadCourse = createAsyncThunk(
-  'player/load',
-  async () => {
-    const response = await api.get('/courses/1')
+export const loadCourse = createAsyncThunk("player/load", async () => {
+  const response = await api.get("/courses/1");
 
-    return response.data
-  }
-)
+  return response.data;
+});
 
 export const playerSlice = createSlice({
   name: "player",
   initialState,
   reducers: {
     play: (state, action: PayloadAction<[number, number]>) => {
-      state.currentModuleIndex = action.payload[0]
-      state.currentLessonIndex = action.payload[1]
+      state.currentModuleIndex = action.payload[0];
+      state.currentLessonIndex = action.payload[1];
     },
     next: (state) => {
       const nextLessonIndex = state.currentLessonIndex + 1;
-      const nextLesson = state.course?.modules[state.currentModuleIndex].lessons[nextLessonIndex];
+      const nextLesson =
+        state.course?.modules[state.currentModuleIndex].lessons[
+          nextLessonIndex
+        ];
 
       if (nextLesson) {
         state.currentLessonIndex = nextLessonIndex;
@@ -60,25 +62,29 @@ export const playerSlice = createSlice({
           state.currentLessonIndex = 0;
         }
       }
-    }
+    },
   },
   extraReducers(builder) {
+    builder.addCase(loadCourse.pending, (state) => {
+      state.isLoading = true;
+    });
     builder.addCase(loadCourse.fulfilled, (state, action) => {
-      state.course = action.payload
-    })
+      state.course = action.payload;
+      state.isLoading = false;
+    });
   },
-})
+});
 
 export const player = playerSlice.reducer;
 export const { play, next } = playerSlice.actions;
 
 export const useCurrentLesson = () => {
-  return useAppSelector(state => {
-    const { currentModuleIndex, currentLessonIndex } = state.player
+  return useAppSelector((state) => {
+    const { currentModuleIndex, currentLessonIndex } = state.player;
 
-    const currentModule = state.player.course?.modules[currentModuleIndex]
-    const currentLesson = currentModule?.lessons[currentLessonIndex]
+    const currentModule = state.player.course?.modules[currentModuleIndex];
+    const currentLesson = currentModule?.lessons[currentLessonIndex];
 
-    return { currentModule, currentLesson }
-  })
-}
+    return { currentModule, currentLesson };
+  });
+};
